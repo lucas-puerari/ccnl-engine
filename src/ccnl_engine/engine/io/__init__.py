371d804c52ad743d2f91e82be106dd1193fc3e87
lucas-puerari <puerari.lucas@gmail.com>
2026-09-19 10:51:00 +0200

    fix(knowledge): unify resource store for source and wheel

--- src/ccnl_engine/engine/io/__init__.py
+++ src/ccnl_engine/engine/io/__init__.py
@@ -1,5 +1,6 @@
 """I/O utilities for reading bundled package data files."""
 
 from ccnl_engine.engine.io.service.bundled import read_bundled
+from ccnl_engine.engine.io.service.bundled_resources import BundledResourceStore
 
-__all__ = ["read_bundled"]
+__all__ = ["BundledResourceStore", "read_bundled"]
